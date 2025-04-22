d9cfa7bc74429e1ddc91e8cca1f03af1f53e7138
doq <darrenangelo.fernandez.cics@Ust.edu.ph>
2025-04-22 12:24:50 +0800

    created database

---
+++ app.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2025 at 06:24 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ganapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_eventstracker`
+--
+
+CREATE TABLE `tbl_eventstracker` (
+  `eventID` int(11) NOT NULL,
+  `eventName` varchar(50) NOT NULL,
+  `eventDateTime` datetime NOT NULL,
+  `eventVenue` varchar(50) NOT NULL,
+  `orgID` varchar(50) NOT NULL,
+  `eventInformation` varchar(250) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_orgs`
+--
+
+CREATE TABLE `tbl_orgs` (
+  `orgID` int(11) NOT NULL,
+  `orgName` varchar(50) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_roles`
+--
+
+CREATE TABLE `tbl_roles` (
+  `roleID` int(11) NOT NULL,
+  `roleName` varchar(50) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_userinformation`
+--
+
+CREATE TABLE `tbl_userinformation` (
+  `userID` int(11) NOT NULL,
+  `firstName` varchar(150) NOT NULL,
+  `lastName` varchar(150) NOT NULL,
+  `contactNumber` varchar(12) NOT NULL,
+  `emailAddress` varchar(50) NOT NULL,
+  `roleID` int(11) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_eventstracker`
+--
+ALTER TABLE `tbl_eventstracker`
+  ADD PRIMARY KEY (`eventID`);
+
+--
+-- Indexes for table `tbl_orgs`
+--
+ALTER TABLE `tbl_orgs`
+  ADD PRIMARY KEY (`orgID`);
+
+--
+-- Indexes for table `tbl_roles`
+--
+ALTER TABLE `tbl_roles`
+  ADD PRIMARY KEY (`roleID`);
+
+--
+-- Indexes for table `tbl_userinformation`
+--
+ALTER TABLE `tbl_userinformation`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_eventstracker`
+--
+ALTER TABLE `tbl_eventstracker`
+  MODIFY `eventID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_orgs`
+--
+ALTER TABLE `tbl_orgs`
+  MODIFY `orgID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_roles`
+--
+ALTER TABLE `tbl_roles`
+  MODIFY `roleID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_userinformation`
+--
+ALTER TABLE `tbl_userinformation`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
