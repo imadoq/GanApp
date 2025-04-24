a842a37a4ee9f997a6ff0adf6bbc2242e5296b1a
doq <darrenangelo.fernandez.cics@Ust.edu.ph>
2025-04-24 10:19:58 +0800

    test

--- APP/Main.php
+++ APP/Main.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src ="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <script src ="js/main.js"></script>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
   </head>
   <body>
@@ -26,7 +27,7 @@
             <h1>GanApp</h1>
             <p class="subtitle">Event Tracker</p>
             
-            <form method="post" action="UserPage.php">
+            <form method="" action="">
                 <div class="input-container">
                     <i class="fas fa-envelope input-icon"></i>
                     <input type="text" name="username" placeholder="Username" required>
@@ -35,7 +36,7 @@
                     <i class="fas fa-lock input-icon"></i>
                     <input type="password" name="password" placeholder="Password" required>
                 </div>
-                <button type="submit" class="submit-btn">
+                <button type="button" class="submit-btn" onclick="sendJSON()">
                     Login
                 </button>
             </form>
